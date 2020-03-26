50daf08c7a861892480011a7866ee31eb11164c2
jimmcgaw <jpmcgaw@gmail.com>
2020-03-26 14:29:27 -0700

    update

--- supplyrunners/settings.py
+++ supplyrunners/settings.py
@@ -121,16 +121,28 @@ WSGI_APPLICATION = 'supplyrunners.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'supplyrunners',
-        'HOST': '127.0.0.1',
-        'USER': 'sambrannan',
-        'PASSWORD': 'calistoga',
-        'PORT': 5432,
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'supplyrunners',
+            'HOST': '127.0.0.1',
+            'USER': 'sambrannan',
+            'PASSWORD': 'calistoga',
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
