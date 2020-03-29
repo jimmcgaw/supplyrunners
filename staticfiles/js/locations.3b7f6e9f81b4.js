d7d1548f161bcd4462eabd69495ab82988ae6fcb
jimmcgaw <jpmcgaw@gmail.com>
2020-03-29 13:03:18 -0700

    adjust static files

---
+++ staticfiles/js/locations.3b7f6e9f81b4.js
@@ -0,0 +1,225 @@
+// var map;
+// var markers = [];
+// var infoWindows = [];
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 37.7790262, lng: -122.4199061},
+//         zoom: 13
+//     });
+// }
+
+
+// start $(function(){})
+window.onload = function() {
+
+// var locationTemplate = document.getElementById('locationtemplate');
+
+// search elements
+var searchForm = $('form#search')
+var searchThrobber = $('#search-throbber');
+var searchButton = $('#search-button');
+
+// search results elements
+var results = $('#results');
+var resultsEmpty = $('#results-empty');
+var resultsError = $('#results-error');
+
+// modal element
+var locationModal = $('#locationModal');
+
+// my locations
+var myLocations = $('#my-locations');
+var myLocationsEmpty = $('#my-locations-empty');
+var myLocationsLoading = $('#my-locations-throbber');
+
+
+function createLocationResultElement(data){
+
+    var locationResultTemplate = $('#location-result-template');
+    var clone = locationResultTemplate.clone().contents();
+    clone.find('.name').text(data.name);
+    clone.find('.address').text(data.formatted_address);
+    clone.find('.place_id').val(data.place_id);
+    return clone;
+}
+
+function createLocationElement(data) {
+    var locationTemplate = $('#location-template');
+    var clone = locationTemplate.clone().contents();
+    clone.find('.name').text(data.name)
+    clone.find('.address').text(data.address)
+    clone.find('.place_id').val(data.place_id)
+    return clone;
+}
+
+function clearResults(){
+    results.html('');
+}
+
+function onLoadResults(data){
+    hideSearchProgress();
+    clearResults();
+
+    data.candidates.map(createLocationResultElement).forEach(function(locationElement){
+        results.append(locationElement);
+    });
+}
+
+function onLoadResultsError(data){
+    resultsError.show();
+    hideSearchProgress();
+}
+
+function getJsonResponse(response){
+    return response.json().then(onLoadResults);
+}
+
+function showSearchProgress(){
+    searchButton.hide();
+    searchThrobber.show();
+}
+
+function hideSearchProgress(){
+    searchButton.show();
+    searchThrobber.hide();
+}
+
+function executeSearch(url){
+    showSearchProgress();
+
+    fetch(url)
+        .then(getJsonResponse);
+}
+
+
+searchForm.off('submit').on('submit', function(e){
+    e.preventDefault();
+
+    var form = $(e.target);
+    var query = form.find('input#query').val();
+    if (query && query.length < 3){
+        return;
+    }
+
+    var params = {'q': query };
+    var latitude = Cookies.get('latitude');
+    var longitude = Cookies.get('longitude');
+    if (latitude && longitude) {
+        params['latitude'] = latitude;
+        params['longitude'] = longitude;
+    }
+    var url = form.attr('action') + '?' + $.param(params);
+    executeSearch(url);
+});
+
+
+function onLocationLoadSuccess(data) {
+    myLocationsLoading.hide();
+    if (data.locations.length === 0){
+        myLocationsEmpty.show();
+    } else {
+        data.locations.map(createLocationElement).forEach(function(locationElement){
+            myLocations.append(locationElement);
+        });
+    }
+}
+
+
+function onLocationLoadError(data) {
+    console.error(data);
+}
+
+
+results.off('submit').on('submit', '.location-result form', function(e){
+    e.preventDefault();
+
+    var form = $(e.target);
+    var url = form.attr('action');
+    var method = form.attr('method');
+    var params = {
+        'place_id': form.find('input.place_id').val(),
+        'csrfmiddlewaretoken': form.find('[name="csrfmiddlewaretoken"]').val()
+    };
+    $.ajax(url, {
+        data: params,
+        method: method,
+        success: loadMyLocations,
+        error: onLocationLoadError
+    });
+});
+
+
+function onDeleteLocationError(e){
+    console.error('Error deleting location');
+    console.error(e);
+}
+
+
+myLocations.off('submit').on('submit', '.location form', function(e){
+    e.preventDefault();
+
+    var form = $(e.target);
+    var url = form.attr('action');
+    var method = form.attr('method');
+    var params = {
+        'place_id': form.find('input.place_id').val(),
+        'csrfmiddlewaretoken': form.find('[name="csrfmiddlewaretoken"]').val()
+    };
+    $.ajax(url, {
+        data: params,
+        method: method,
+        success: loadMyLocations,
+        error: onDeleteLocationError
+    });
+})
+
+
+function onLocationSuccess(position){
+    Cookies.set('latitude', position.coords.latitude);
+    Cookies.set('longitude', position.coords.longitude);
+    Cookies.set('location-checked', new Date().toString());
+}
+
+function onLocationError(){
+    console.info('geolocation denied.');
+    Cookies.set('location-checked', new Date().toString());
+}
+
+function checkLocation(){
+    if (navigator && 'geolocation' in navigator){
+        navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
+    }
+}
+
+locationModal.on('hidden.bs.modal', function (e) {
+    checkLocation();
+});
+
+if (!Cookies.get('location-checked')){
+    if (navigator && navigator.geolocation){
+        locationModal.modal();
+    }
+}
+
+function clearMyLocations() {
+    myLocations.html('');
+}
+
+function loadMyLocations(){
+    clearMyLocations();
+    myLocationsLoading.show();
+    myLocationsEmpty.hide();
+
+    $.ajax('/locations/', 
+        {
+            success: onLocationLoadSuccess,
+            error: onLocationLoadError
+        }
+    );
+}
+
+loadMyLocations();
+
+
+// end $(function(){})
+}
